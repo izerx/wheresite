53d766638c67b2a197f40c9defe7c8c71579a4bf
Roman <romshtmb@gmail.com>
2019-10-20 16:31:02 +0300

    add model of result

--- App/models.py
+++ App/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Results(models.Model):
+    url = models.CharField(max_length=300, blank=True, null=True)
+    save_url = models.CharField(max_length=300, blank=True, null=True)
+    points = models.CharField(max_length=10000, blank=True, null=True) 
